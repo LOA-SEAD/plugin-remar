e92762e3d13107661690d8319c01a33f39527aa7
Rener Baffa da Silva <renerbaffa@gmail.com>
2016-03-10 15:16:08 -0300

    Exibindo tabela de dados novamente

--- table.php
+++ table.php
@@ -5,47 +5,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 require_once(dirname(__FILE__) . '/../../config.php');
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 
-global $DB, $USER;
+//global $DB, $USER;
 
-$records = $DB->get_records("remarmoodle_escola_magica", array("remar_resource_id" => $_GET['resourceid']));
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => "http://localhost:9090/moodle/getLogFromResource",
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => array(
+        "resourceId" => $_POST['resourceid'],
+    )
+));
+
+$json = curl_exec($curl);
+curl_close($curl);
+
+$obj = json_decode($json);
+
+$records = $obj->data;
 
 if ($records == null) {
     echo html_writer::label("Ainda não há dados", null);
 }
 else {
-    echo "<br />";
     $table = new html_table();
-    
-    $table->head = array(
-        'Usuário', /*'Módulo do curso', 'Atividade REMAR',*/ 'Enunciado',
-        'Alternativa A', 'Alternativa B', 'Alternativa C', 'Alternativa D',
-        'Resposta Correta', 'Resposta Escolhida', 'Data'
-    );
-    
-    $data = array();
-    
+    $head = array();
+    $data;
+    $count = 0;
+
     foreach($records as $record) {
-        if ($record->user_id != 0) {
-            $currentUser = $DB->get_record('user', array('id' => $record->user_id));
-            
-            $organized_array['user'] = $currentUser->firstname." ".$currentUser->lastname;
-            //$organized_array['cm'] = $record->cm;
-            //$organized_array['remar_resource_id'] = $record->remar_resource_id;
-            $organized_array['enunciado'] = $record->enunciado;
-            $organized_array['alternativaa'] = $record->alternativaa;
-            $organized_array['alternativab'] = $record->alternativab;
-            $organized_array['alternativac'] = $record->alternativac;
-            $organized_array['alternativad'] = $record->alternativad;
-            $organized_array['respostacerta'] = $record->respostacerta;
-            $organized_array['resposta'] = $record->resposta;
-            $organized_array['timestamp'] = date("d/m/Y H:i:s", $record->timestamp);
-            array_push($data, $organized_array);
+        foreach($record as $key=>$value) {
+            if (is_string($value) && $key != "moodle_url") {
+                if($count == 0) {
+                    array_push($head, $key);
+                }
+                $data[$count][$key] = $value;
+            }
         }
+        $count++;
     }
+
+    $table->head = $head;
+    
     $table->data = $data;
     
     echo html_writer::table($table);
